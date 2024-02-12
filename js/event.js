5f6a819f19c456a16d5def894f4977d78254d5b7
Idba1 <moniraislamself@gmail.com>
2024-02-12 18:59:23 +0600

    make some event using by onclick

--- js/event.js
+++ js/event.js
@@ -1,1 +1,28 @@
-console.log('This is separate js file');
+// console.log('This is separate js file');
+
+
+// Option : 1 Directly set on the HTML element
+
+
+//option:2 add on click
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option : 3
+const makeBlueButton = document.getElementById('make-blue');
+// console.log(makeBlueButton);
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+
+// option : 3 Another way
+const purpleButton = document.getElementById('make-purple');
+//console.log(purpleButton);
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
